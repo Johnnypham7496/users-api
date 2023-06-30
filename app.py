5df24d4785b08e1efbecab194750b70cafbe10dd
Johnnpham7496 <johnpham7496@gmail.com>
2023-06-29 21:24:09 -0500

    created hello world endpoint with using the home.html template

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
-# using packages from venv by telling this file where is it and what to import 
-from flask import Flask, Response
-# creating a variable
+from flask import Flask, render_template
+
+
 app = Flask(__name__)
 
-# this is the address to the webpage. If you were to change the string inside route to /johnny then it would route this to http://localhost:5000/johnny 
-# with just '/' means that the web address ends with '/' = http://localhost:5000'/'
-# App Routing means mapping the URLs to a specific function that will handle the logic for that URL
-@app.route('/')    
-# create or define a function
-def welcome():
-    response_text = '{"message": "Hello, welcome to Johnny\'s flask-api"}' 
-    response = Response(response_text, 200, mimetype='applications/json')  # returning the reponse type as json 
-    return response
+
+@app.route("/")
+def home():
+    return render_template("home.html")
 
 
-# if statement to run app it '__name__' meets the matches '__main__'
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000, debug=True) # port numbers can only be used once so will not be able to use 5000 again, debug is set to True to let users know about errors if there are any
+    app.run(host='localhost', port=8000, debug=True)
