43a9ba7af38cdfa4b7b7fee5d0300386c4684ef8
Johnnpham7496 <johnpham7496@gmail.com>
2023-07-03 13:05:06 -0500

    changed yaml to yml

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import render_template
 import connexion
 
 app = connexion.App(__name__, specification_dir= "./")
-app.add_api("swagger.yaml")
+app.add_api("swagger.yml")
 
 
 @app.route("/")
