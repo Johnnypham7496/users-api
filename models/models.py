a2a94cd2e36af6ac6f6b2d669478320c39b5489b
Johnnpham7496 <johnpham7496@gmail.com>
2023-07-17 22:50:31 -0500

    added PersonSchema class to models.py

--- models/models.py
+++ models/models.py
@@ -1,7 +1,7 @@
 # User models are used to generate or adapt user interfaces at runtime, to address particular user needs and preferences. 
 # User models are also known as user profiles, personas or archetypes
 from datetime import datetime
-from config import db
+from config import db, ma
 
 
 class Person(db.Model):
@@ -10,3 +10,13 @@ class Person(db.Model):
     lname = db.Column(db.String(100), unique=True)
     fname = db.Column(db.String(100), unique=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+
+class PersonSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model= Person
+        load_instance = True
+        sqla_session = db.session
+
+person_schema = PersonSchema()
+person_schema = PersonSchema(many=True)
