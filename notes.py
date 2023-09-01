b31f0a15ade0c37f87966c03ea54170591b162f9
Johnnpham7496 <johnpham7496@gmail.com>
2023-08-31 19:22:23 -0500

    added create note function in notes.py and fixed changed operationID for create_note in swagger.yml

--- notes.py
+++ notes.py
@@ -1,6 +1,23 @@
 from flask import abort, make_response
 from config import db
-from models.models import Note, note_schema
+from models.models import Note, note_schema, Person
+
+
+def create_note(note):
+    person_id = note.get("person_id")
+    person = Person.query.get(person_id)
+
+    if person:
+        new_note = note_schema.load(note, session= db.session)
+        person.notes.append(new_note)
+        db.session.commit()
+        return note_schema.dump(new_note), 201
+
+    else:
+        abort(
+            406,
+            f"Person not found for ID: {person_id}"
+        )
 
 
 def read_one(note_id):
